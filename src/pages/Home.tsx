9abe575923c9f599169662360a62f6cfcbd826d0
kimchicadny <nsr7025@gmail.com>
2026-05-03 18:39:13 +0900

    feat(home): assemble all 7 sections in order

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,25 @@
-export default function Home() { return <div className="p-8">Home</div>; }
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Topics from "../components/Topics";
+import TalkList from "../components/TalkList";
+import YouTubeEmbed from "../components/YouTubeEmbed";
+import ProductCards from "../components/ProductCards";
+import Contact from "../components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Topics />
+      <TalkList compact />
+      <YouTubeEmbed />
+      <ProductCards compact />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
